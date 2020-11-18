92d55110c7c5f99fbbb5d1b2460e7689ae23e47a
Amar Sawant <amar.t.sawant@gmail.com>
2020-11-18 11:39:18 +0530

    initial commit

---
+++ Weather/Model/WeatherData.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherData.swift
+//  Weather
+//
+//  Created by Amar Sawant on 18/11/20.
+//  Copyright © 2020 atsawant.com. All rights reserved.
+//
+
+import Foundation
---
+++ Weather/Services/ServiceManager.swift
@@ -0,0 +1,9 @@
+//
+//  ServiceManager.swift
+//  Weather
+//
+//  Created by Amar Sawant on 18/11/20.
+//  Copyright © 2020 atsawant.com. All rights reserved.
+//
+
+import Foundation
